96014b53031e22a3e4f55387eed081c3e46dfd61
Blake Mason <blakelmason@gmail.com>
2020-02-28 15:28:37 -0800

    lazy

--- client/src/data/hr-projects.js
+++ client/src/data/hr-projects.js
@@ -1,5 +1,59 @@
 const projects = [
   {
+    name: 'Poké Hub',
+    url: 'https://poke-hub.herokuapp.com/',
+    github: 'https://github.com/blakes-gym',
+    tools: [
+      'HTML',
+      'CSS',
+      'JavaScript',
+      'Node',
+      'Bootstrap',
+      'React',
+      'SQL',
+      'PostgreSQL',
+      'Heroku',
+      'Material-UI',
+      'Next'
+    ],
+    description: `Full stack final project built in one week. An application to build a wish list of pokemon and perform a team analysis of strengths and weaknesses.`
+  },
+  {
+    name: 'Checkout Process',
+    url: 'https://checkout-hr.herokuapp.com/',
+    github: 'https://github.com/blakelmason/checkout',
+    tools: [
+      'HTML',
+      'CSS',
+      'JavaScript',
+      'Node',
+      'Express',
+      'Bootstrap',
+      'React',
+      'SQL',
+      'MySQL',
+      'Heroku',
+      'Webpack'
+    ],
+    description: `Basic forms and conditional rendering with React.`
+  },
+  {
+    name: 'Connect Four',
+    url: 'https://connect-four-hr.herokuapp.com/',
+    github: 'https://github.com/blakelmason/connect-four',
+    tools: [
+      'HTML',
+      'CSS',
+      'JavaScript',
+      'Node',
+      'Express',
+      'Bootstrap',
+      'React',
+      'Heroku'
+    ],
+    description: `Play Connect Four with a friend.`
+  },
+  {
     name: 'JSON to CSV',
     url: 'https://json-flattener.herokuapp.com/',
     github: 'https://github.com/blakelmason/json-flattener',
@@ -21,8 +75,7 @@ const projects = [
       'SQL',
       'MySQL',
       'Heroku',
-      'Webpack',
-      'Babel'
+      'Webpack'
     ],
     description:
       'A simple full stack grocery list application with CRUD operations.'
